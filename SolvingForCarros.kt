cf2de976477f1fc3f1b961b76cb7de30aec521fd
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-07-31 14:51:56 +0300

    Add files via upload

---
+++ SolvingForCarros.kt
@@ -0,0 +1,23 @@
+package com.borapocan.heartrate
+
+class SolvingForCarrots {
+}
+
+fun main() {
+
+    val input = readLine()!!.split(' ').map { it.toInt() }
+
+    val n = input[0]
+
+    val p = input[1]
+
+    for (i in 1..n) {
+
+        val readline = readLine()!!.toString()
+
+    }
+
+    println(p)
+
+
+}
