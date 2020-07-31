6d115a320528fed5975340ad47f211ea1c8ebdbc
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-07-31 14:50:15 +0300

    Add files via upload

---
+++ TheLastProblem.kt
@@ -0,0 +1,13 @@
+package com.borapocan.heartrate
+
+class TheLastProblem {
+}
+
+fun main() {
+
+    val input = readLine()!!.toString()
+
+    val output = "Thank you, $input, and farewell!"
+
+    println(output)
+}
