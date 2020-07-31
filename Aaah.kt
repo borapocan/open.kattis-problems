b1e593ff8bb1c93650280fc35163a4c0c2b3f6a7
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-07-31 14:49:56 +0300

    Add files via upload

---
+++ Aaah.kt
@@ -0,0 +1,27 @@
+package com.borapocan.heartrate
+
+class Aaah {
+}
+fun main() {
+
+    var readline = readLine()!!.toString()
+
+    var input = readLine()!!.toString()
+
+    if (input.length in 1..1000) {
+
+        if (readline.length < input.length) {
+
+            println("no")
+
+        }else {
+
+            println("go")
+
+        }
+
+
+    }
+
+
+}
